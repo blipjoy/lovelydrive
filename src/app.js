31a39f1032c627735049483936c9ca6faae89d6a
Jay Oster <jay@kodewerx.org>
2015-08-22 02:41:02 -0700

    Add some basic procedural texture generation

--- src/app.js
+++ src/app.js
@@ -97,6 +97,178 @@ gl.uniformMatrix4fv(
     ])
 )
 
+// Deterministic random function
+/*random = function () {
+    var x = Math.PI
+    return function () {
+        x += Math.E
+        return x % 1
+    }
+}()//*/
+
+// Sinusoidal tween
+function easeInOut(value) {
+    return .5 * (1 - Math.cos(Math.PI * value))
+}
+
+// Height map function
+function height(value, weight) {
+    return Math.max(Math.min(value + (Math.random() - .5) * weight, 1), 0)
+}
+
+// Generate fractal mountains
+var n = 128
+var d = []
+for (var i = 0; i <= n; i++) {
+    d[i] = []
+}
+
+// diamond-square worker queue
+var q = []
+
+function diamond(x, y, w, s, f) {
+    d[x + w / 2][y + w / 2] = f((
+        d[x][y] +
+        d[x + w][y] +
+        d[x][y + w] +
+        d[x + w][y + w]
+    ) / 4, s)
+}
+
+function subsquare(x, y, w, n, s, f) {
+    d[x][y] =
+    // FIXME: This assignment makes the factral wrap properly. How can I make this smaller?
+    d[!x ? n : x == n ? 0 : x][!y ? n : y == n ? 0 : y] =
+    f((
+        d[(x - w + n) % n][y] +
+        d[x][(y - w + n) % n] +
+        d[(x + w) % n][y] +
+        d[x][(y + w) % n]
+    ) / 4, s)
+}
+
+function square(x, y, w, n, s, f) {
+    subsquare(x + w / 2, y, w / 2, n, s, f)
+    subsquare(x, y + w / 2, w / 2, n, s, f)
+    subsquare(x + w, y + w / 2, w / 2, n, s, f)
+    subsquare(x + w / 2, y + w, w / 2, n, s, f)
+
+    // Update fractal state
+    w /= 2
+    s /= 2
+
+    // Recurse
+    if (w > 1) {
+        // Perform diamond steps
+        diamond(x, y, w, s, f)
+        diamond(x + w, y, w, s, f)
+        diamond(x, y + w, w, s, f)
+        diamond(x + w, y + w, w, s, f)
+
+        // Push square steps onto a worker queue
+        q.push([ x, y, w, n, s, f ])
+        q.push([ x + w, y, w, n, s, f ])
+        q.push([ x, y + w, w, n, s, f ])
+        q.push([ x + w, y + w, w, n, s, f ])
+    }
+}
+
+// Setup initial state
+d[0][0] = d[0][n] = d[n][0] = d[n][n] = .3 // Corners
+
+d[n / 2][n / 2] = .5 // Center: 0 for clouds, 1 for mountains
+square(0, 0, n, n, 2, height)
+
+// Empty worker queue
+while (q.length) {
+    square.apply(square, q.shift())
+}
+
+
+// Procedural texture generation
+m = 1024
+var ctx = document.createElement("canvas").getContext("2d")
+ctx.canvas.width = ctx.canvas.height = m
+var img = ctx.createImageData(m, m)
+
+// XXX: <DEBUG>
+document.body.appendChild(ctx.canvas)
+// XXX: </DEBUG>
+
+
+// RENDER CLOUDS
+tmp = 0
+for (var y = 0; y < m; y++) {
+    for (var x = 0; x < m; x++) {
+        // Interpolate between each node
+        var sx = x / m * n
+        var sy = y / m * n
+
+        var p1 = d[~~sx][~~sy]
+        var p2 = d[~~sx + 1][~~sy]
+        var p3 = d[~~sx][~~sy + 1]
+        var p4 = d[~~sx + 1][~~sy + 1]
+
+        var h = (
+            (p1 + (p2 - p1) * (sx % 1)) * (1 - sy % 1) +
+            (p1 + (p3 - p1) * (sy % 1)) * (1 - sx % 1) +
+            (p3 + (p4 - p3) * (sx % 1)) * (sy % 1) +
+            (p2 + (p4 - p2) * (sy % 1)) * (sx % 1)
+        ) / 2
+
+        // Choose a color based on node height
+        img.data[tmp++] = 102 + h * 118 // 102 -> 220
+        img.data[tmp++] = 145 + h * 55 // 145 -> 200
+        img.data[tmp++] = 220
+        img.data[tmp++] = 255
+    }
+}
+
+
+// Setup initial state
+/*
+d[0][0] = d[0][n] = d[n][0] = d[n][n] = .3 // Corners
+
+d[n / 2][n / 2] = 1 // Center: 0 for clouds, 1 for mountains
+square(0, 0, n, n, 2, height)
+
+// Empty worker queue
+while (q.length) {
+    square.apply(square, q.shift())
+}
+*/
+
+// RENDER MOUNTAINS
+for (var y = 0; y <= n; y++) {
+    for (var x = 0; x < m; x++) {
+        // Interpolate between each node
+        var sx = x / m * n
+        var p1 = d[~~sx][y]
+        var p2 = d[~~sx + 1][y]
+        var j = (p2 - p1) * (sx % 1)
+
+        // Compute the node height
+        var h = ~~(m - (p1 + j) * m * .4 * (((n - y) / n) + .3))
+
+        // Draw a vertical strip with the computed height
+        while (h <= m) {
+            tmp = (h * m + x) * 4
+
+            // Choose a color based on depth
+            img.data[tmp]     = (n - y * .4) / n * 200//230
+            img.data[tmp + 1] = (n - y * .6) / n * 184//222
+            img.data[tmp + 2] = 222
+            img.data[tmp + 3] = 255
+
+            h++
+        }
+    }
+}
+
+
+ctx.putImageData(img, 0, 0)
+
+
 // Render loop
 function raf() {
     // Animation state
