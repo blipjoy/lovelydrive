8b91f16259f3bbefa0d9330534c47d893b952140
Jay Oster <jay@kodewerx.org>
2015-08-22 17:36:46 -0700

    Faster mountain generator
    - Use Canvas 2D Path API instead of pixel manipulation
    - The new height mapping also tends to generate better mountains:
      - Distant ridges are scaled bigger, and drawn higher from the bottom
      - Close ridges are scaled down, and drawn lower

--- src/app.js
+++ src/app.js
@@ -175,8 +175,7 @@ function square(x, y, w, n, s, f) {
 
 // Setup initial state
 d[0][0] = d[0][n] = d[n][0] = d[n][n] = .3 // Corners
-
-d[n / 2][n / 2] = .5 // Center: 0 for clouds, 1 for mountains
+diamond(0, 0, n, 2, height)
 square(0, 0, n, n, 2, height)
 
 // Empty worker queue
@@ -223,50 +222,49 @@ for (var y = 0; y < m; y++) {
         img.data[tmp++] = 255
     }
 }
+ctx.putImageData(img, 0, 0)
 
 
 // Setup initial state
-/*
 d[0][0] = d[0][n] = d[n][0] = d[n][n] = .3 // Corners
-
-d[n / 2][n / 2] = 1 // Center: 0 for clouds, 1 for mountains
+diamond(0, 0, n, 2, height)
 square(0, 0, n, n, 2, height)
 
 // Empty worker queue
 while (q.length) {
     square.apply(square, q.shift())
 }
-*/
+
 
 // RENDER MOUNTAINS
 for (var y = 0; y <= n; y++) {
-    for (var x = 0; x < m; x++) {
-        // Interpolate between each node
-        var sx = x / m * n
-        var p1 = d[~~sx][y]
-        var p2 = d[~~sx + 1][y]
-        var j = (p2 - p1) * (sx % 1)
+    ctx.beginPath()
 
-        // Compute the node height
-        var h = ~~(m - (p1 + j) * m * .4 * (((n - y) / n) + .3))
+    // Choose a color based on depth
+    // FIXME: Use fragment shader to add fog (based on depth)
+    ctx.fillStyle =
+        "rgb(" +
+        ~~((n - y * .4) / n * 200) + "," +
+        ~~((n - y * .6) / n * 184) + "," +
+        "222)"
 
-        // Draw a vertical strip with the computed height
-        while (h <= m) {
-            tmp = (h * m + x) * 4
+    ctx.moveTo(0, m)
 
-            // Choose a color based on depth
-            img.data[tmp]     = (n - y * .4) / n * 200//230
-            img.data[tmp + 1] = (n - y * .6) / n * 184//222
-            img.data[tmp + 2] = 222
-            img.data[tmp + 3] = 255
-
-            h++
-        }
+    for (var x = 0; x <= n; x++) {
+        // When (y == 0): highest point scaled to 0.4, lowest scaled to 0.8
+        // When (y == n): highest point scaled to 0.7, lowest scaled to 0.9
+        ctx.lineTo(x / n * m,
+            (
+                (1 - d[x][y]) *
+                (((n - y) / n) * .4 + (y / n) * .2) +
+                (((n - y) / n) * .4 + (y / n) * .7)
+            ) * m
+        )
     }
+
+    ctx.lineTo(m, m)
+    ctx.fill()
 }
-
-
-ctx.putImageData(img, 0, 0)
 
 
 // Render loop
